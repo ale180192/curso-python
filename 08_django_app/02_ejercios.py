669e9b213908539f018cbe3e85928b2a98b52208
Alejandro Lujan <alejandro.lujan.aldaba@gmail.com>
2021-08-09 01:25:28 -0500

    sd

---
+++ 08_django_app/02_ejercios.py
@@ -0,0 +1,15 @@
+"""
+Usando como base el proyecto visto anteriormente crear una nueva app ecommerce la cual debe tener
+las siguientes caracteristicas:
+
+* Tener un modelo Producto y un modelo Carrito
+* Crear los siguientes endpoints:
+    - Usando el shell interactivo de django crear productos.
+    - Un endpoint GET para poder listar todos los productos que existan en la db
+    - Un endpoint POST que permita crear un carrito de compras
+    - Un endpoint PUT que permita agregar productos al carrito de compra(investigar como funcionan las foreing keys en los modelos de django)
+    - Un enpoint GET que regrese toda la informacion resumen del carrito(total, subtotal, impuestos, total de productos)
+
+Validar que funcionen correctamente todos los endopints haciendo un flujo completo con
+todos los endopints.
+"""
